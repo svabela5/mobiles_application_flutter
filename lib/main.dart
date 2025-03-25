eb6a529c48ed8398eaf6e3d2ed8ae14b122deb65
svabela5 <109699629+svabela5@users.noreply.github.com>
2025-03-25 15:26:59 +0100

    Started implementing main page

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      title: 'Phone Arena',
+      theme: ThemeData.dark().copyWith(
+        
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
