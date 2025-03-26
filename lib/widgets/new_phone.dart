ab3a43be59d041da9c6653736ccd324c0b6be27e
svabela5 <109699629+svabela5@users.noreply.github.com>
2025-03-26 14:14:21 +0100

    Created the add phone functionality

--- lib/widgets/new_phone.dart
+++ lib/widgets/new_phone.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobiles_application_flutter/models/phone.dart';
 
 class NewPhone extends StatefulWidget {
   const NewPhone({Key? key}) : super(key: key);
@@ -12,7 +16,7 @@ class NewPhone extends StatefulWidget {
 class _NewPhoneState extends State<NewPhone> {
   final _formKey = GlobalKey<FormState>();
   var _enteredBrand = '';
-  var _enteredModel = 1;
+  var _enteredModel = '';
   var _enteredPrice = 0.00;
 
   var isSendingData = false;
@@ -24,6 +28,29 @@ class _NewPhoneState extends State<NewPhone> {
       setState(() {
         isSendingData = true;
       });
+
+      final url = Uri.https('phone-arena-flutter-default-rtdb.firebaseio.com', 'phones.json');
+
+      final response = await http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            'brand': _enteredBrand,
+            'model': _enteredModel,
+            'price': _enteredPrice,
+          }));
+
+      Map<String, dynamic> responseData = json.decode(response.body);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop(Phone(
+          id: responseData['name'],
+          brand: _enteredBrand,
+          model: _enteredModel,
+          price: _enteredPrice,
+          ));
     }
   }
 
@@ -39,23 +66,75 @@ class _NewPhoneState extends State<NewPhone> {
           key: _formKey,
           child: Column(
             children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  //Brand
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Brand'),
+                      ),
+                      initialValue: '',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Must be between 1 and 50 characters';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredBrand = value!;
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  //Model
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Model'),
+                      ),
+                      initialValue: '',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Must be between 1 and 50 characters';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredModel = value!;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //Price
               TextFormField(
                 maxLength: 50,
                 decoration: const InputDecoration(
-                  label: Text('Brand'),
+                  label: Text('Price  (€)'),
                 ),
+                initialValue: '0.00',
                 validator: (value) {
                   if (value == null ||
                       value.isEmpty ||
-                      value.trim().length <= 1 ||
-                      value.trim().length > 50) {
-                    return 'Must be between 1 and 50 characters';
+                      double.tryParse(value) == null ||
+                      double.tryParse(value)! < 0) {
+                    return 'Must be a number greater than 0';
                   }
                   return null;
                 },
                 onSaved: (value) {
-                  _enteredBrand = value!;
+                  _enteredPrice = double.tryParse(value!)!;
                 },
+              ),
+              const SizedBox(
+                height: 10,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
