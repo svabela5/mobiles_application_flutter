945772cd59816b0b9a9f80db99477e19b16e7a72
svabela5 <109699629+svabela5@users.noreply.github.com>
2025-03-26 09:47:46 +0100

    Preping project to read from local data

---
+++ lib/models/phone.dart
@@ -0,0 +1,13 @@
+class Phone {
+  const Phone({
+    required this.id,
+    required this.brand,
+    required this.model,
+    required this.price,
+  });
+
+  final String id;
+  final String brand;
+  final String model;
+  final double price;
+}
